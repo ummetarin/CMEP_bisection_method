5ff5e2ae4805e9dd80bcaab4a370eb7b6bfa4a1b
ummetarin <ummehomairatarin@gmail.com>
2024-05-13 23:41:26 +0600

    Define error and function

--- Bisection_method.c
+++ Bisection_method.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+// define error like decimal
+#define E 0.001
+// define a function
+#define F(x) ((x*x*x)-5*x+3)
+
 int main(){
+
+
+
+
 
 }
